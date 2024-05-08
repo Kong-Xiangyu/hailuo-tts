1cbda72cc5f29d7ec518811ec31b71d564cfe1a5
kX=Y <68771395+Kong-Xiangyu@users.noreply.github.com>
2024-05-09 00:49:52 +0800

    Update audio.ts

--- src/api/controllers/audio.ts
+++ src/api/controllers/audio.ts
@@ -12,6 +12,9 @@ const MODEL_NAME = "hailuo";
 // 语音生成异步锁
 const voiceLock = new AsyncLock();
 
+// 历史发言人
+let historyVoiceID = '';
+
 /**
  * 创建语音
  *
@@ -25,10 +28,26 @@ async function createSpeech(
   voice: string,
   token: string
 ) {
-  // 先由hailuo复述语音内容获得会话ID和消息ID
-  const answer = await chat.createRepeatCompletion(model, input.replace(/\n/g, '。'), token);
-  const { id: convId, message_id: messageId } = answer;
-
+  const prefix = '[VMSG]';
+  let convId = '', messageId = '';
+  // 检查字符串是否以特定标识符开始
+  if (input.startsWith(prefix)) {
+     // 移除特定标识符，然后按分隔符分割剩余字符串
+     const parts = input.substring(prefix.length).split('|');
+     // 检查是否有两个UUID部分
+     if (parts.length === 2) {
+      convId = parts[0];
+      messageId = parts[1];
+     }
+  }
+  // 直接进行 tts
+  if (!convId || !messageId) {
+    // 先由hailuo复述语音内容获得会话ID和消息ID
+    const answer = await chat.createRepeatCompletion(model, input.replace(/\n/g, '。'), token);
+    const { id, message_id } = answer;
+    convId = id;
+    messageId = message_id;
+  }
   const deviceInfo = await core.acquireDeviceInfo(token);
 
   // OpenAI模型映射转换
@@ -36,18 +55,21 @@ async function createSpeech(
     voice = modelMap[model][voice] || voice;
 
   const audioUrls = await voiceLock.acquire(token, async () => {
-    // 请求切换发音人
-    const result = await core.request(
-      "POST",
-      "/v1/api/chat/update_robot_custom_config",
-      {
-        robotID: "1",
-        config: { robotVoiceID: voice },
-      },
-      token,
-      deviceInfo
-    );
-    core.checkResult(result);
+    if (!historyVoiceID || historyVoiceID !== voice) {
+      // 请求切换发音人
+      const result = await core.request(
+        "POST",
+        "/v1/api/chat/update_robot_custom_config",
+        {
+          robotID: "1",
+          config: { robotVoiceID: voice },
+        },
+        token,
+        deviceInfo
+      );
+      core.checkResult(result);
+      historyVoiceID = voice;
+    }
 
     // 请求生成语音
     let requestStatus = 0,
